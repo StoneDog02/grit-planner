7625a983e8aa0db05580e07b562f917ce12dbfcf
Stoney Harward <stoney.harward@gmail.com>
2025-03-10 15:46:45 -0600

    lambda error fixes for deployment

--- netlify/functions/server.js
+++ netlify/functions/server.js
@@ -1,19 +1,31 @@
-const { createRequestHandler } = require("@remix-run/netlify");
-const build = require("@remix-run/dev/server-build");
+import { createRequestHandler } from "@remix-run/netlify";
+import * as build from "@remix-run/dev/server-build";
 
-const handler = createRequestHandler({
+const requestHandler = createRequestHandler({
   build,
   mode: process.env.NODE_ENV,
 });
 
-exports.handler = async (event, context) => {
+export const handler = async (event, context) => {
   try {
-    return await handler(event, context);
+    const response = await requestHandler(event, context);
+    // Ensure we always return a valid response
+    return {
+      ...response,
+      statusCode: response.statusCode || 200,
+      body: response.body || "",
+    };
   } catch (error) {
     console.error("Error in server function:", error);
     return {
       statusCode: 500,
-      body: JSON.stringify({ error: "Internal Server Error" }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        error: "Internal Server Error",
+        message: error.message,
+      }),
     };
   }
 };
